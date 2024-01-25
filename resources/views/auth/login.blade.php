805f57733317c479b00e9c53776a139f3f0373ca
nurbismii <nurbismi74@gmail.com>
2024-01-25 14:08:55 +0800

    test: penyesuaian btn login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.auth')
 
 @section('content')
-
+<style>
+    #loader {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        background-image: none;
+        background: rgba(0, 0, 0, 0.75) url("{{ asset('assets/img/loader/loader-horizontal-unscreen.gif') }}") no-repeat center center;
+        z-index: 99999;
+    }
+</style>
 <div class="container-xxl">
     <div class="authentication-wrapper authentication-basic container-p-y">
         <div class="authentication-inner">
@@ -25,7 +38,7 @@
                             <label for="email" class="form-label">Email</label>
                             <input type="text" class="form-control" id="email" name="email" placeholder="Masukan email kamu" autofocus required />
                         </div>
-                        <div class="mb-3 form-password-toggle">
+                        <div class="mb-4 form-password-toggle">
                             <div class="d-flex justify-content-between">
                                 <label class="form-label">Kata sandi</label>
                             </div>
@@ -34,13 +47,13 @@
                                 <span toggle="#password-field" class="input-group-text cursor-pointer bx bx-hide toggle-password"></span>
                             </div>
                         </div>
+                        <div class="mb-3">
+                            <button class="btn btn-primary d-grid w-100 from-prevent-multiple-submits" aria-hidden="true" type="submit">Masuk</button>
+                        </div>
                     </div>
-                    <div class="mb-3">
-                        <button class="btn btn-primary d-grid w-100 from-prevent-multiple-submits" id="load" data-loading-text="<div class='spinner-border text-primary' role='status'>
-                        <span class='visually-hidden'>Loading...</span>
-                    </div>" aria-hidden="true" type="submit">Masuk</button>
-                    </div>
+                </div>
             </form>
+            <div class="loader"></div>
         </div>
     </div>
 </div>
